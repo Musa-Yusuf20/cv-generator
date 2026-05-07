20122e6a31b71967f2973292f27531b710ac92fc
Muse <musa2012@hotmail.co.uk>
2026-05-07 12:40:39 +0100

    Added view,url to connect to dashboard home page, updated the save_profile view logic.

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path("",views.save_profile,name="save_profile"),
+    path("",views.dashboard,name="dashboard"),
+    path("saveprofile",views.save_profile,name="save_profile"),
     path("<int:id>/",views.resume,name="resume"),
+    
 ]
